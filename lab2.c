79eb262c485fba3c8be2bfe9235946f210761ca5
oscar trombini <oscar@KalyHost.Trombs>
2024-05-15 20:04:41 +0200

    15/05

---
+++ lab2.c
@@ -0,0 +1,54 @@
+// progetto lab2 dei files
+#include <sys/stat.h> // necessario per stat
+#include <dirent.h> // necessario per struct dirent*
+#include <stdlib.h> // necessario per exit
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+void ric(const char *arg) {
+
+    struct dirent* dirp;
+    struct stat buf;
+    // viene aperta la cartella e restituisce un puntatore di tipo DIR *
+    DIR *d = opendir(arg);
+
+    // apro la dir e inizio il ciclo while 
+    while ((dirp = readdir(d)) != NULL) {
+        // creo una var dove salvare il path
+        char path[1000];
+        
+        if(strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0) 
+            continue;
+
+        // ora path sara il percorso della libreria
+        snprintf(path, sizeof(path), "%s/%s", arg, dirp->d_name);
+
+        // stat prende il percorso passato e le colloca nella struct stat buf ()
+        if (stat(path, &buf) < 0) {
+            perror("stat");
+            continue;
+        }
+
+        printf("%s\n", path);
+            
+        // se e una directory entro nella funzione path
+        if(S_ISDIR(buf.st_mode))
+            ric(path);
+    }
+
+    closedir(d);
+
+}
+
+int main(int argc, char * argv[]) {
+    
+    if(argc!=2)
+        return 1;
+
+    ric(argv[1]);
+   
+return 0;
+
+}
